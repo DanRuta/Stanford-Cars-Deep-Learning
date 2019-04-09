ce37915078cb0e5538b28d6131307b3df3e5120c
Dan Ruta <ruta_dan_sebastian@yahoo.co.uk>
2019-04-09 12:52:10 +0100

    Reduced output dataset size, unnecessary duplications, and output stats

--- preProcessData.py
+++ preProcessData.py
@@ -5,8 +5,21 @@ import argparse
 import math
 import random
 
+import numpy as np
+import cv2 as cv
+# https://forums.fast.ai/t/image-normalization-in-pytorch/7534/6
 
-def preprocess(overwriteData=False):
+
+
+def preprocess(overwriteData=False, skipWritingFiles=False):
+
+    totalImages = 0
+    meanR = 0
+    meanG = 0
+    meanB = 0
+    stdR = 0
+    stdG = 0
+    stdB = 0
 
     labels = []
     classes = []
@@ -17,23 +30,25 @@ def preprocess(overwriteData=False):
         classes = [x for x in f.read().split("\n") if x is not ""]
 
 
-    # Check if the output has already been created, and skip this
-    if os.path.exists("data") is False:
-        os.mkdir("data")
-        os.mkdir("data/augmentations")
-        prepareDataFolder(classes, splits)
+    if not skipWritingFiles:
 
-    elif os.path.exists("./data/augmentations"):
-        if not overwriteData:
-            print("Data has already been pre-processed.")
-            return
-        else:
-            # Clear out the folder
-            print("Clearing out old data")
+        # Check if the output has already been created, and skip this
+        if os.path.exists("data") is False:
+            os.mkdir("data")
+            os.mkdir("data/augmentations")
             prepareDataFolder(classes, splits)
 
+        elif os.path.exists("./data/augmentations"):
+            if not overwriteData:
+                print("Data has already been pre-processed.")
+                return
+            else:
+                # Clear out the folder
+                print("Clearing out old data")
+                prepareDataFolder(classes, splits)
 
-    print("Pre-processing data")
+
+        print("Pre-processing data")
 
     with open("data_pre/labels.txt") as f:
         labels = [x.split(",") for x in f.read().split("\n")]
@@ -51,9 +66,24 @@ def preprocess(overwriteData=False):
             imagePath = "./data_pre/images/{}".format(imgName)
             classification = labels[l][1]
 
+            image = cv.imread(imagePath)
+            totalImages += 1
+            b, g, r = image[:, :, 0], image[:, :, 1], image[:, :, 2]
 
+
+            meanR += np.mean(r)
+            meanG += np.mean(g)
+            meanB += np.mean(b)
+            stdR += np.std(r)
+            stdG += np.std(g)
+            stdB += np.std(b)
+
+
+            if skipWritingFiles:
+                continue
+
+            newImg = cv.resize(image, (int(224*1.5), int(224*1.5)))
             for [training, validation, test] in splits:
-                # print([training, validation, test])
 
                 r = random.random()*100
                 splitFolder = "{}-{}-{}".format(training, validation, test)
@@ -69,13 +99,32 @@ def preprocess(overwriteData=False):
                 else:
                     folder = "val"
 
-                shutil.copy(imagePath, "./data/{}/{}/{}/{}".format(splitFolder, folder, classification, imgName))
+
+                cv.imwrite("./data/{}/{}/{}/{}".format(splitFolder, folder, classification, imgName), newImg)
 
                 # Copy each image 10 times, to allow plenty of different transformations to happen, to each one
-                for i in range(0, 10):
-                    shutil.copy(imagePath, "./data/augmentations/{}/{}/{}/{}_{}".format(splitFolder, folder, classification, i, imgName))
+                if folder=="train":
+                    for i in range(0, 10):
+                        cv.imwrite("./data/augmentations/{}/{}/{}/{}_{}".format(splitFolder, folder, classification, i, imgName), newImg)
+                else:
+                    # The validation and test data does not get augmented, and therefore only 1 copy of each image is needed
+                    cv.imwrite("./data/augmentations/{}/{}/{}/{}_{}".format(splitFolder, folder, classification, i, imgName), newImg)
 
-    print("\nFinished pre-processing data")
+    print("\nFinished pre-processing data ({} images)".format(totalImages))
+
+    meanR /= totalImages
+    meanG /= totalImages
+    meanB /= totalImages
+    stdR /= totalImages
+    stdG /= totalImages
+    stdB /= totalImages
+
+    print("meanR: {} ({})".format(meanR, meanR/255))
+    print("meanG: {} ({})".format(meanG, meanG/255))
+    print("meanB: {} ({})".format(meanB, meanB/255))
+    print("stdR: {} ({})".format(stdR, stdR/255))
+    print("stdG: {} ({})".format(stdG, stdG/255))
+    print("stdB: {} ({})".format(stdB, stdB/255))
 
 
 def prepareDataFolder (classes, splits):
@@ -112,6 +161,7 @@ if __name__=="__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--o', default=False, type=bool)
+    parser.add_argument('--sw', default=False, type=bool, help="Skip file writing")
     args = parser.parse_args()
 
-    preprocess(args.o)
+    preprocess(args.o, args.sw)
